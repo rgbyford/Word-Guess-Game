f1ff534df7668060d18934023ce1865abb53add8
Roger <rgbyford@gmail.com>
2018-08-24 13:28:52 -0500

    first working version

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,45 @@
     // put all the stuff on the screen
 
     let guesses = 0;
-    let guessWord = "dogdd";
+    let guessWord = new Array("doggy", "leopard", "tiger", "elephant", "monkey", "cat");
     let partialGuess = [];
     let guessLetters = [];
     let correctGuesses = 0;
+    let wordArray = [];
+    let originalWord = [];
+    let wins = 0;
+    let newGame = true;
 
-    document.getElementById("demo").addEventListener("keypress", keyPressed);
-    console.log("Press a key");
-    for (let i = 0; i < guessWord.length; i++) {
-        partialGuess[i] = '_';
-    }
-    console.log(partialGuess);
-
+    //    console.log(partialGuess);
+    
+    
     function keyPressed(event) {
+        let removed = 0;
+
         event = event || window.event; //capture the event, and ensure we have an event
-        var key = event.key || event.which || event.keyCode; //find the key that was pressed
-        //        console.log("key pressed!");
-        var x = event.key;
+        //        var key = event.key || event.which || event.keyCode; //find the key that was pressed
+        let x = event.key;
+        if (newGame === true) {
+            partialGuess = [];
+            for (let i = 0; i < guessWord[wins].length; i++) {
+                partialGuess[i] = '_';
+            }
+            if (wins >= guessWord.length) {
+                wins = 0;
+            }
+            wordArray = guessWord[wins].split('');
+            originalWord = guessWord[wins].split('');
+            correctGuesses = 0;
+            guessLetters = [];
+            guesses = 0;
+            document.getElementById("start").innerHTML = partialGuess.join(' ');
+            document.getElementById("guesses").value = "";
+            document.getElementById("again").innerHTML = "";
+            document.getElementById("guessesLeft").innerHTML = "Guesses left:  " + (9 - guesses);
+            document.getElementById("gotIt").innerHTML = "";
+            newGame = false;
+            return;
+        }
 
         if (guesses++ >= 9) {
             console.log("Too many guesses!");
@@ -26,46 +48,42 @@
         }
 
         if ((x >= "A" && x <= "Z") || (x >= "a" && x <= "z")) {
+            document.getElementById("warning").innerHTML = " ";
             console.log("You pressed an alpha");
-            //        if (it's in the array of guesses) {
-            //            print "Already guessed!";
-            //            display yucky emoji
-            //        }
-            //            if (guessLetters.includes (x)) {
-            //                console.log("Already guessed");
+            //            if (guessLetters.indexOf(x) >= 0) {
+            //                console.log("Already guessed!");
             //            } else {
             guessLetters.push(x);
-            let iCharPos = 0;
-            let iLast = 0;
-            while (iCharPos != -1) {
-                console.log (guessWord.slice(iLast));
-                iCharPos = guessWord.slice(iLast).indexOf(x);
-                if (iCharPos >= 0) {
-                    partialGuess[iLast + iCharPos] = x;
-                    correctGuesses++;
-                    iLast += iCharPos + 1;
-                    if (iLast >= guessWord.length) {
-                        break;          // guessed the last letter
-                    }
-                }
+            let charIndex = wordArray.indexOf(x);
+            if (charIndex >= 0) {
+                let track = 0;
+                wordArray.splice(charIndex, 1);
+                track = originalWord.indexOf(x);
+                originalWord[track] = '_';
+                partialGuess[track] = x;
+                removed++;
+                correctGuesses++;
             }
-            //                console.log (guessWord.split(x).length - 1);
-
-            //                if (guessWord.includes(x)) {
-            //correctGuesses++;
-            //                    partialGuess[guessWord.indexOf(x)] = x;
             console.log(partialGuess);
-            //                }
-            if (correctGuesses === guessWord.length) {
-                document.writeln(guessWord);
-                document.writeln("Got it!");
+            if (correctGuesses === guessWord[wins].length) {
+                document.getElementById("gotIt").innerHTML = "You got it!";
+                //                document.writeln(guessWord);
+                //                document.writeln("Got it!");
+                document.getElementById("guesses").value = "";
+                document.getElementById("again").innerHTML = "Press any key to play again";
+                newGame = true;
+                wins++;
             }
-        }
-        else {
+        } else {
+            document.getElementById("warning").innerHTML = "Only letters!";
             console.log("Only alphas!")
             //        display yucky emoji;
             //        continue;
         }
+        document.getElementById("start").innerHTML = partialGuess.join(' ');
+        document.getElementById("wins").innerHTML = "Wins:  " + wins;
+        document.getElementById("guessesLeft").innerHTML = "Guesses left:  " + (9 - guesses);
+
         return;
     }
 
